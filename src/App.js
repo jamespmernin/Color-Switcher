945dbc9a476e935a900338a2228696dd049d3445
James Mernin <jamespmernin@gmail.com>
2020-10-07 13:36:14 -0400

    Part 1 done

--- src/App.js
+++ src/App.js
@@ -5,22 +5,26 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      color: "black"
+    };
+  }
 
-    }
+  changeColor(newColor) {
+    this.setState({ color: newColor });
   }
 
   render() {
     return (
       <div className="app">
         <h1 style={{ color: "black" }}>Color Switcher</h1>
-        <div id="color-panel" className="black"></div>
+        <div id="color-panel" className={this.state.color}></div>
         <div className="button-container">
-          <button className="red"></button>
-          <button className="yellow"></button>
-          <button className="blue"></button>
-          <button className="green"></button>
+          <button className="red" onClick={() => this.changeColor("red")}></button>
+          < button className="yellow" onClick={() => this.changeColor("yellow")}></button>
+          <button className="blue" onClick={() => this.changeColor("blue")}></button>
+          <button className="green" onClick={() => this.changeColor("green")}></button>
         </div>
-        <input type="color"/>
+        <input type="color" />
       </div>
     );
   }
